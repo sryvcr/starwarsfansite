c8aafb2cf2b4482d20b555c2e52ead952aaaaafc
sryvcr <andresgx9@gmail.com>
2020-07-27 19:03:12 -0500

    feature/models and init data

---
+++ starwarsfansite/characters/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=100)
+    climate = models.CharField(max_length=50)
+    terrain = models.CharField(max_length=100)
+    population = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    episode_id = models.IntegerField()
+    opening_crawl = models.TextField()
+    release_year = models.IntegerField()
+    director = models.CharField(max_length=100)
+    producers = models.CharField(max_length=200)
+    planets = models.ManyToManyField(
+        Planet,
+        related_name='movies',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.CharField(max_length=20)
+    genre = models.CharField(max_length=10)
+    movies = models.ManyToManyField(
+        Movie,
+        related_name='characters',
+    )
+
+    def __str__(self):
+        return self.name
